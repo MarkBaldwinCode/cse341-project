8d21cb35c1f624f21ae1c264af7231746ff01d47
Mark Baldwin <baldwin.mv05@gmail.com>
2021-09-22 21:14:10 -0600

    New dev branch after team meeting week 2

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,29 @@
 const express = require('express');
 const router = express.Router();
 
+let newUsers = ['Nick'];
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users: newUsers
   });
 });
+
+  router.post('/addUser', (req, res, next) => {
+    newUsers.push(req.body.userName);
+    //console.log(req.body.userName);
+    res.writeHead(302, {Location: '/ta02'});
+    return res.end();
+  })
+
+  router.post('/removeUser', (req, res, next) => {
+    newUsers = newUsers.filter(user => user !== req.body.removeUserName);
+    res.writeHead(302, {Location: '/ta02'});
+    return res.end();
+  })
 
 module.exports = router;
